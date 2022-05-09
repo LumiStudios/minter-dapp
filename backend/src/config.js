6bccdaf25eed13edb23304b523f19d112216c7e2
LumiStudios <toluwasheojoo@gmail.com>
2022-05-09 11:33:42 +0100

    white list for rinkeby

--- backend/src/config.js
+++ backend/src/config.js
@@ -68,7 +68,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = ["0x3d7045c772e819A81e439a9354abCf11d9741534"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0xBb8FABb6E8796a83Fe4dACA777F553a8330E881b"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x98229183DA02BCD099A61D6C1e3B83A0b69BA3b4"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
